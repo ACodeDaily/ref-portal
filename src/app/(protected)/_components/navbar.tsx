b0de56c8cbb64e56bd72a3e4e7fff53eedbb3529
Dhruvil Kakadiya <kakadiyadhruvil3006@gmail.com>
2024-02-03 14:29:38 +0530

    Made responsive navbar which was causing issue in responsiveness

--- src/app/(protected)/_components/navbar.tsx
+++ src/app/(protected)/_components/navbar.tsx
@@ -7,7 +7,7 @@ import { useCurrentRole } from '@/src/hooks/use-currrent-role'
 import { UserRole } from '@prisma/client'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 import { useTransition } from "react";
 import Image from "next/image";
 import { RoleGateForComponent } from '@/src/components/auth/role-gate-component'
@@ -16,17 +16,15 @@ import { Form, FormField, FormControl, FormItem, FormLabel, FormDescription, For
 import { useForm } from "react-hook-form";
 import { Input } from "@/src/components/ui/input";
 import { toast } from "sonner";
-
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import { organizationAddSchema } from "@/src/schemas";
 import { organizationAddition } from "@/src/actions/organization";
 
 
 
-
-
-
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
 
     const form = useForm({
         defaultValues: {
@@ -59,122 +57,208 @@ const Navbar = () => {
 
     const pathname = usePathname();
     return (
-        <nav className='bg-secondary flex justify-between items-center p-4 rounded-xl w-[90%] shadow-sm'>
-            <div className='flex gap-x-2'>
+        <>
+            <nav className='bg-secondary p-4 rounded-xl w-[90%] shadow-sm'>
+                <div className="flex justify-between items-center ">
+                    <div className='flex gap-x-2 max-md:hidden'>
 
-                <div className='mt-[-1px]'>
-                    <Image
-                        // onClick={() => router.push('/')}
-                        src="/images/TextLogo.png"
-                        height="80"
-                        width="80"
-                        alt="Logo"
-                    />
-                </div>
+                        <div className='mt-[-1px]'>
+                            <Image
+                                src="/images/TextLogo.png"
+                                height="80"
+                                width="80"
+                                alt="Logo"
+                            />
+                        </div>
 
-                <Button
-                    asChild
-                    variant={pathname === "/member" ? "default" : "outline"}
-                >
-                    <Link href="/member">Members</Link>
-                </Button>
-
-
-                <RoleGateForComponent allowedRole={[UserRole.ADMIN, UserRole.MOD]}>
-                    <Button
-                        asChild
-                        variant={pathname === "/referrer" ? "default" : "outline"}
-                    >
-                        <Link href="/referrer">Referrer</Link>
-                    </Button>
-                </RoleGateForComponent>
-
-                <RoleGateForComponent allowedRole={[UserRole.ADMIN, UserRole.MOD]}>
-                    <Button
-                        asChild
-                        variant={pathname === "/request" ? "default" : "outline"}
-                    >
-                        <Link href="/request">Requests</Link>
-                    </Button>
-                </RoleGateForComponent>
-
-                <RoleGateForComponent allowedRole={[UserRole.ADMIN]}>
-                    <Button
-                        asChild
-                        variant={pathname === "/moderator" ? "default" : "outline"}
-                    >
-                        <Link href="/moderator">Moderator</Link>
-                    </Button>
-                </RoleGateForComponent>
-
-                <Button
-                    asChild
-                    variant={pathname === "/settings" ? "default" : "outline"}
-                >
-                    <Link href="/settings">Settings</Link>
-                </Button>
-
-
-            </div>
-            <div className="flex justify-between gap-x-2">
-
-                <RoleGateForComponent allowedRole={[UserRole.ADMIN]}>
-                    <Button
-                        asChild
-                        variant={pathname === "/organizations" ? "default" : "outline"}
-                    >
-                        <Link href="/organizations">Organizations</Link>
-                    </Button>
-                </RoleGateForComponent>
-
-                <RoleGateForComponent allowedRole={[UserRole.ADMIN]}>
-                    <Button
-                        asChild
-                    >
-                        <DialogDemo
-                            dialogTrigger="Add Organization"
-                            dialogTitle="New Organization Addition"
-                            dialogDescription="give organization name and click add organization"
+                        <Button
+                            asChild
+                            variant={pathname === "/member" ? "default" : "outline"}
                         >
+                            <Link href="/member">Members</Link>
+                        </Button>
 
-                            <Form {...form}>
-                                <form
-                                    className="space-y-6"
-                                    onSubmit={form.handleSubmit(onSubmit)}
+
+                        <RoleGateForComponent allowedRole={[UserRole.ADMIN, UserRole.MOD]}>
+                            <Button
+                                asChild
+                                variant={pathname === "/referrer" ? "default" : "outline"}
+                            >
+                                <Link href="/referrer">Referrer</Link>
+                            </Button>
+                        </RoleGateForComponent>
+
+                        <RoleGateForComponent allowedRole={[UserRole.ADMIN, UserRole.MOD]}>
+                            <Button
+                                asChild
+                                variant={pathname === "/request" ? "default" : "outline"}
+                            >
+                                <Link href="/request">Requests</Link>
+                            </Button>
+                        </RoleGateForComponent>
+
+                        <RoleGateForComponent allowedRole={[UserRole.ADMIN]}>
+                            <Button
+                                asChild
+                                variant={pathname === "/moderator" ? "default" : "outline"}
+                            >
+                                <Link href="/moderator">Moderator</Link>
+                            </Button>
+                        </RoleGateForComponent>
+
+                        <Button
+                            asChild
+                            variant={pathname === "/settings" ? "default" : "outline"}
+                        >
+                            <Link href="/settings">Settings</Link>
+                        </Button>
+
+
+                    </div>
+                    <div className="flex gap-x-2 md:hidden">
+                        <button
+                            className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                            onClick={() => setNavbar(!navbar)}
+                        >
+                            {navbar ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6 text-balck"
+                                    viewBox="0 0 20 20"
+                                    fill="black"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6 text-balck"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="black"
+                                    strokeWidth={2}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
+                    <div className='flex gap-x-2 md:hidden'>
+                        <div className='mt-[-1px]'>
+                            <Image
+                                // onClick={() => router.push('/')}
+                                src="/images/TextLogo.png"
+                                height="80"
+                                width="80"
+                                alt="Logo"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between gap-x-2">
+
+                        <RoleGateForComponent allowedRole={[UserRole.ADMIN]}>
+                            <Button
+                                asChild
+                                variant={pathname === "/organizations" ? "default" : "outline"}
+                            >
+                                <Link href="/organizations">Organizations</Link>
+                            </Button>
+                        </RoleGateForComponent>
+
+                        <RoleGateForComponent allowedRole={[UserRole.ADMIN]}>
+                            <Button
+                                asChild
+                            >
+                                <DialogDemo
+                                    dialogTrigger="Add Organization"
+                                    dialogTitle="New Organization Addition"
+                                    dialogDescription="give organization name and click add organization"
                                 >
 
-                                    <FormField
-                                        control={form.control}
-                                        name="name"
-                                        render={({ field }) => (
-                                            <FormItem>
-                                                <FormLabel>Organization</FormLabel>
-                                                <FormControl>
-                                                    <Input
-                                                        {...field}
-                                                        disabled={isPending}
-                                                        placeholder="enter organization name"
+                                    <Form {...form}>
+                                        <form
+                                            className="space-y-6"
+                                            onSubmit={form.handleSubmit(onSubmit)}
+                                        >
 
-                                                    />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        )}
-                                    />
+                                            <FormField
+                                                control={form.control}
+                                                name="name"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel>Organization</FormLabel>
+                                                        <FormControl>
+                                                            <Input
+                                                                {...field}
+                                                                disabled={isPending}
+                                                                placeholder="enter organization name"
+
+                                                            />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
 
 
-                                    <Button type="submit" disabled={isPending}>Add Organization</Button>
-                                </form>
-                            </Form>
+                                            <Button type="submit" disabled={isPending}>Add Organization</Button>
+                                        </form>
+                                    </Form>
 
-                        </DialogDemo>
+                                </DialogDemo>
 
-                    </Button>
-                </RoleGateForComponent>
-                <UserButton />
-            </div>
+                            </Button>
+                        </RoleGateForComponent>
+                        <UserButton />
+                    </div>
+                </div>
+                <div className="md:hidden">
+                    <div
+                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden'
+                            }`}
+                    >
+                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            <li className="text-balck">
+                                <Link href="/">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="text-balck">
+                                <Link href="/member">
+                                    Members
+                                </Link>
+                            </li>
+                            <li className="text-balck">
+                                <Link href="/referrer">
+                                    Referrer
+                                </Link>
+                            </li>
+                            <li className="text-balck">
+                                <Link href="/request">
+                                    Requests
+                                </Link>
+                            </li>
+                            <li className="text-balck">
+                                <Link href="/settings">
+                                    Settings
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav >
 
-        </nav >
+
+        </>
     )
 }
 
