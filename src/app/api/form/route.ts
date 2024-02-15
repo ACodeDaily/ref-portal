d97d83b64015404d2d6f7f569897a9fc65c91264
Dhruvil Kakadiya <kakadiyadhruvil3006@gmail.com>
2024-02-16 01:05:56 +0530

    resolved conflicts everything working fine

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import { getALlMember, getAllMembersWithForm } from "@/src/data/member";
-=======
+
 import { getAllMembersWithForm, getAllMembersWithFormByOrganizationAndRefferer } from "@/src/data/member";
->>>>>>> b5330f3594273f41ee419e4f2055a4969a0b7da5
 import { currentRole, currentUser } from "@/src/lib/auth";
 import { UserRole } from "@prisma/client";
 import { NextResponse } from "next/server";
@@ -24,8 +21,6 @@ export async function GET() {
         }), { status: 200 })
     }
 
-<<<<<<< HEAD
-=======
     else if (role === UserRole.REFERRER) {
         if (user?.email && user.organization !== undefined){
             const data = await getAllMembersWithFormByOrganizationAndRefferer(user?.organization, user?.id);
@@ -39,7 +34,6 @@ export async function GET() {
         }
         
     }
->>>>>>> b5330f3594273f41ee419e4f2055a4969a0b7da5
     return new NextResponse(null, {
         status: 403
     });
